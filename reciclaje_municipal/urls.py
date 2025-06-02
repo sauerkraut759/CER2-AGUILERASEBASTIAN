5c051fe7127674456fd987f8dd597df329b1acda
Khaseis <sebaguilera153@gmail.com>
2025-06-02 05:49:03 -0400

    rutas

--- reciclaje_municipal/urls.py
+++ reciclaje_municipal/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('register/', views.register, name="register"),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('retiro/', views.registrarSolicitud, name="registro-solicitud")
 ]
